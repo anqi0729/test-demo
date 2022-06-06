fb819fd9e96a92e63aeabcaf4e18f5d59ece1ba0
AnQi <qianf@isoftstone.com>
2022-06-06 11:44:05 +0800

    github第一次提交

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * index controller
+ * @author xuxueli 2015-12-19 16:13:16
+ */
+@Controller
+public class IndexController {
+
+
+
+
+	@RequestMapping("/home")
+//	@ResponseBody
+	public String index(Model model) {
+		System.out.println("gogogo");
+		return "index";
+	}
+
+
+	
+}
